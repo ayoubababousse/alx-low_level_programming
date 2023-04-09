ac7b8d433a75763b047ddd66001eb5cd926cf5c6
Ayoub <ayoub@gmail.com>
2023-04-09 11:32:38 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to the console
+ * @str: the string to be printed
+ */
+void _puts(char *str);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+/**
+ * _puts - prints a string to the console
+ * @str: the string to be printed
+ */
+void _puts(char *str)
+{
+puts(str);
+}
